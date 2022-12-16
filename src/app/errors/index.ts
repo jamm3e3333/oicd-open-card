422c89b6ae95d7318cb97033517b1c71e1609efb
Jakub Vala <jakub.vala@plu4u.net>
2022-12-16 01:30:11 +0100

    🥅 Catch cardRequest errors

--- src/app/errors/index.ts
+++ src/app/errors/index.ts
@@ -17,6 +17,14 @@ export const E_CODES = setCustomErrors({
   U4002: {
     code: 'U4002',
     message:
-      'Invalid password. Password must contain minimum eight characters, must have at least one uppercase letter, one lowercase letter and one number',
+      'Invalid password. Password must contain minimum eight characters, must have at least one uppercase letter, one lowercase letter, at least one number and no special character.',
+  },
+  C4000: {
+    code: 'C4000',
+    message: 'Invalid card number. Card number must be integer.',
+  },
+  C5000: {
+    code: 'C5000',
+    message: 'Open card service unavailable',
   },
 } as const)
